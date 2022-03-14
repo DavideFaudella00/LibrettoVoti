a985d4a2c0bee7617993a2162d58b2d6849fff36
david <david@172.22.96.79>
2022-03-14 10:19:01 +0100

    V2

--- src/main/java/it/polito/tdp/librettovoti/FXMLController.java
+++ src/main/java/it/polito/tdp/librettovoti/FXMLController.java
@@ -2,24 +2,58 @@ package it.polito.tdp.librettovoti;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import Model.Libretto;
+import Model.Voto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class FXMLController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+public class FXMLController {
+
+	private Libretto model;
+
+	@FXML
+	private ResourceBundle resources;
+
+	@FXML
+	private URL location;
+
+	@FXML
+	private ComboBox<Integer> cmbPunti;
+
+	@FXML
+	private TextField txtNome;
+
+	@FXML
+	private TextArea txtVoti;
+
+	@FXML
+	void HandleNuovoVoto(ActionEvent event) {
+		String nome = txtNome.getText();
+		int punti = cmbPunti.getValue();
+		model.add(new Voto(nome, punti));
+		String contenutoLibretto = model.toString();
+		txtVoti.setText(contenutoLibretto);
+		
+	}
+
+	public void setModel(Libretto model) {
+		this.model = model;
+	}
+
+	@FXML
+	void initialize() {
+		assert cmbPunti != null : "fx:id=\"cmbPunti\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert txtVoti != null : "fx:id=\"txtVoti\" was not injected: check your FXML file 'Scene.fxml'.";
+		cmbPunti.getItems().clear();
+		for (int i = 18; i <= 30; i++) {
+			cmbPunti.getItems().add(i);
+		}
+	}
+
 }
